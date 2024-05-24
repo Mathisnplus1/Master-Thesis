06039dcf179c1654de9b751628d1bb3ceaa9741e
Mathisnplus1 <mathis.achats74@gmail.com>
2024-05-24 11:58:12 +0200

    Fixed the way we load data in CL framework

--- Workspace/LoRA/lib/dataloader.py
+++ Workspace/LoRA/lib/dataloader.py
@@ -3,35 +3,52 @@ from torch.utils.data import DataLoader, Subset, random_split
 from torchvision import datasets, transforms
 
 
+
 MNIST_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 CIFAR_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 
-def get_MNIST_loaders(path, class_names, batch_size, download=False) :
+def get_MNIST_loaders(path, batch_size, random_seed, train_percentage=0.7, download=False) :
 
-    # load MNIST 
+    train_loaders_list, val_loaders_list, test_loaders_list = [], [], []
+
+    # LOAD MNIST 
     mnist_train = datasets.MNIST(root=path, train=True, download=download, transform=MNIST_transform)
     mnist_test = datasets.MNIST(root=path, train=False, download=download, transform=MNIST_transform)
 
-
-    # create a mask to filter indices for each label
-    train_mask = torch.tensor([label in class_names for label in mnist_train.targets])
-    test_mask = torch.tensor([label in class_names for label in mnist_test.targets])
-
-    # Create Subset datasets for train, validation, and test
-    train_dataset = Subset(mnist_train, torch.where(train_mask)[0])
+    # SPLIT DATA
+    train_dataset, val_dataset = random_split(mnist_train, [train_percentage, 1-train_percentage], generator=torch.Generator().manual_seed(random_seed))
+    test_mask = torch.tensor([label in range(10) for label in mnist_test.targets])
     test_dataset = Subset(mnist_test, torch.where(test_mask)[0])
 
-    # split train into train & validation
-    train_size = int(0.7 * len(train_dataset))
-    val_size = len(train_dataset) - train_size
-    train_dataset, val_dataset = random_split(train_dataset, [train_size, val_size])
+    # GET LOADERS
+    lists_of_labels = [range(5), [5], [6], [7], [8], [9]]
+    for label_list in lists_of_labels :   
+        # train/val/test_dataset's type is Subset, so can't directly access its targets through .targets
+        train_subset_indices = train_dataset.indices
+        train_subset_targets = [train_dataset.dataset.targets[i] for i in train_subset_indices]
+        val_subset_indices = val_dataset.indices
+        val_subset_targets = [val_dataset.dataset.targets[i] for i in val_subset_indices]
+        test_subset_indices = test_dataset.indices
+        test_subset_targets = [test_dataset.dataset.targets[i] for i in test_subset_indices]
+        # create a mask to filter indices for each label
+        train_mask = torch.tensor([label in label_list for label in train_subset_targets])
+        val_mask = torch.tensor([label in label_list for label in val_subset_targets])
+        test_mask = torch.tensor([label in label_list for label in test_subset_targets])
+        # create Subset datasets
+        train_subset = Subset(train_dataset, torch.where(train_mask)[0])
+        val_subset = Subset(val_dataset, torch.where(val_mask)[0])
+        test_subset = Subset(test_dataset, torch.where(test_mask)[0])
+        # get loaders
+        train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=False, drop_last=True)
+        val_loader = DataLoader(val_subset, batch_size=batch_size, shuffle=False, drop_last=True)
+        test_loader = DataLoader(test_subset, batch_size=batch_size, shuffle=False, drop_last=True)
+        # store loaders in the lists of loaders
+        train_loaders_list.append(train_loader)
+        val_loaders_list.append(val_loader)
+        test_loaders_list.append(test_loader)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
-
-    return train_loader, val_loader, test_loader
+    return train_loaders_list, val_loaders_list, test_loaders_list
 
 def get_FMNIST_loaders(path, class_names, batch_size, download=False) :
 

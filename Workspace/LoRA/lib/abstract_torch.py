69a0d4845d8bc77e6fddc0b25f8d0429897a33e2
mathisnplus1 <mathis.reymond74@gmail.com>
2024-05-27 11:10:54 +0200

    Greedy HPO results

--- Workspace/LoRA/lib/abstract_torch.py
+++ Workspace/LoRA/lib/abstract_torch.py
@@ -2,7 +2,7 @@ import torch
 
 
 def get_device() :
-    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
     return device
 
 def get_loss(loss_name) :

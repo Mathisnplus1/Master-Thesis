548aafa4eafba5621da33ea7d8e0b50b91274c60
mathisnplus1 <mathis.reymond74@gmail.com>
2024-07-19 11:49:08 +0200

    rb

--- Workspace/Core work/HPO_lib/HPOs/greedy_HPO copy.py
+++ Workspace/Core work/HPO_lib/HPOs/greedy_HPO copy.py
@@ -13,6 +13,8 @@ import copy
 
 def objective(model, task_number, HPO_settings, params, method_settings, train_loader, val_loaders_list, device, global_seed, trial) :
 
+    global sto_scores, sto_ewc, sto_model
+
     # Set HPs
     HPs = {}
     try :
@@ -44,9 +46,12 @@ def objective(model, task_number, HPO_settings, params, method_settings, train_l
 
     # Copy the model to perform HPO
     model_copy = copy.deepcopy(model)
+    
+    if method_settings["method_name"] == "EWC" :
+        params_copy = copy.deepcopy(params)
 
     # Train
-    _ = train(model_copy, method_settings, params, HPs, train_loader, device, global_seed)
+    output = train(model_copy, method_settings, params_copy, HPs, train_loader, device, global_seed)
 
     # Test
     test_accs = np.zeros(task_number+1)
@@ -56,6 +61,12 @@ def objective(model, task_number, HPO_settings, params, method_settings, train_l
     
     # Compute score
     score = np.mean(test_accs)
+
+    # Store for EWC
+    if method_settings["method_name"] == "EWC" :
+        sto_ewc.append(output)
+        sto_scores.append(score)
+        sto_model.append(model_copy)
 
     return score
 
@@ -94,12 +105,17 @@ def retrain_and_save_with_best_HPs (model, params, method_settings, best_params,
     if method_settings["method_name"] == "GroHess" :
         overall_masks, _, _ = train(model, method_settings, params, best_HPs, train_loader, device, global_seed, verbose=2)
         return overall_masks
-    if method_settings["method_name"] in ["EWC", "LwF", "Naive baseline"] :
+    if method_settings["method_name"] in ["EWC"] :
+        ewc = train(model, method_settings, params, best_HPs, train_loader, device, global_seed, verbose=2)
+        return ewc
+    if method_settings["method_name"] in ["LwF", "Naive baseline"] :
         train(model, method_settings, params, best_HPs, train_loader, device, global_seed, verbose=2)
 
 
 
 def call_greedy_HPO(HPO_settings, method_settings, benchmark_settings, benchmark, device, global_seed) :
+    
+    global sto_scores, sto_ewc, sto_model
 
     # Unpack loaders
     try :
@@ -110,10 +126,14 @@ def call_greedy_HPO(HPO_settings, method_settings, benchmark_settings, benchmark
 
     # Initialize model
     model = initialize_model(method_settings, global_seed).to(device)
+    hpo_model = copy.deepcopy(model)
 
     # Intialize HPO
     if method_settings["method_name"] == "GroHess" :
         overall_masks = initialize_training(model, method_settings)
+    if method_settings["method_name"] in ["EWC"] :
+        ewc = initialize_training(model, method_settings, benchmark_settings, device)
+        hpo_ewc = initialize_training(model, method_settings, benchmark_settings, device)
     best_params_list = []
     num_tasks = benchmark_settings["num_tasks"]
     test_accs_matrix = np.zeros((num_tasks, num_tasks))
@@ -138,12 +158,19 @@ def call_greedy_HPO(HPO_settings, method_settings, benchmark_settings, benchmark
                 overall_masks = output
             is_first_task = True if task_number==0 else False
             params = {"overall_masks" : overall_masks, "is_first_task" : is_first_task}
-        if method_settings["method_name"] in ["EWC", "LwF"] :
+        if method_settings["method_name"] in ["EWC"] :
+            sto_scores, sto_ewc, sto_model = [], [], []
+            params = {"ewc" : hpo_ewc}
+        if method_settings["method_name"] in ["LwF"] :
             params = {"batch_size" : benchmark_settings["batch_size"]}
-        partial_objective = partial(objective, model, task_number, HPO_settings, params, method_settings, train_loader, val_loaders_list, device, global_seed)
+        partial_objective = partial(objective, hpo_model, task_number, HPO_settings, params, method_settings, train_loader, val_loaders_list, device, global_seed)
         study.optimize(partial_objective,
                     n_trials=HPO_settings["n_trials"],
                     timeout=3600)
+        if method_settings["method_name"] == "EWC" :
+            best_index = np.argmax(sto_scores)
+            hpo_ewc, hpo_model = sto_ewc[best_index], sto_model[best_index]
+
 
         # Retrain and save a model with the best params
         best_params = study.best_trial.params
@@ -152,10 +179,14 @@ def call_greedy_HPO(HPO_settings, method_settings, benchmark_settings, benchmark
             if output is not None :
                 overall_masks = output
             is_first_task = True if task_number==0 else False
-            params = {"overall_masks" : overall_masks, "is_first_task" : is_first_task}
-        if method_settings["method_name"] in ["EWC", "LwF"] :
-            params = {"batch_size" : benchmark_settings["batch_size"]}
-        output = retrain_and_save_with_best_HPs(model, params, method_settings, best_params, train_loader, device, global_seed) 
+            sto_params = {"overall_masks" : overall_masks, "is_first_task" : is_first_task}
+        if method_settings["method_name"] in ["EWC"] :
+            if output is not None :
+                ewc = output
+            sto_params = {"ewc" : ewc}
+        if method_settings["method_name"] in ["LwF"] :
+            sto_params = {"batch_size" : benchmark_settings["batch_size"]}
+        output = retrain_and_save_with_best_HPs(model, sto_params, method_settings, best_params, train_loader, device, global_seed) 
         
 
         # Test on each task
--- Workspace/Core work/Methods/EWC/lib/method.py
+++ Workspace/Core work/Methods/EWC/lib/method.py
@@ -18,50 +18,38 @@ def initialize_model(method_settings, global_seed) :
     return model
 
 
-def initialize_training(model, method_settings, benchmark_settings, device) :
-    try :
-        optimizer_name = method_settings["optimizer_name"]
-        loss_name = method_settings["loss_name"]
-    except ValueError:
-        print("One or more of the required settings to initialize the HPO are missing. Please check the method settings.")
-    try :
-        batch_size = benchmark_settings["batch_size"]
-    except ValueError:
-        print("One or more of the required settings to initialize the HPO are missing. Please check the benchmark settings.")
-
-    ewc = EWC(
-        model=model, 
-        optimizer=get_optimizer(optimizer_name, model),
-        criterion=get_loss(loss_name),
-        ewc_lambda=0,
-        train_mb_size=batch_size,
-        train_epochs=0, 
-        eval_every=-1,
-        device=device
-    )
-
-    return ewc
-
-
 def train (model, method_settings, params, HPs, experience, device, global_seed, verbose=0) :
+    # Get method settings
+    try :
+        loss_name = method_settings["loss_name"]
+        optimizer_name = method_settings["optimizer_name"]
+    except ValueError:
+        print("One or more of the required settings to train the model are missing. Please check the method settings.")
+
+    # Get params
+    try :
+        batch_size = params["batch_size"]
+    except ValueError:
+        print("One or more of the required params to train the model are missing. Please check the params.")
+
     # Get HPs
     try :
         ewc_lambda = HPs["ewc_lambda"]
         num_epochs = HPs["num_epochs"]
     except ValueError:
         print("One or more of the required HPs to train the model are missing. Please check the HPs.")
-    # Get params
-    try :
-        ewc = params["ewc"]
-    except ValueError:
-        print("One or more of the required params to train the model are missing. Please check the params.")
 
     # Train
-    ewc.model = model
-    ewc.optimizer = get_optimizer(method_settings["optimizer_name"], model)
-    ewc.plugins[0].ewc_lambda = ewc_lambda
-    ewc.train_epochs = num_epochs
+    ewc = EWC(
+        model=model, 
+        optimizer=get_optimizer(optimizer_name, model),
+        criterion=get_loss(loss_name),
+        ewc_lambda=ewc_lambda,
+        train_mb_size=batch_size,
+        train_epochs=num_epochs, 
+        eval_every=-1,
+        device=device
+    )
 
     ewc.train(experience)
-
-    return ewc
+    

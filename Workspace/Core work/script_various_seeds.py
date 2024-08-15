7fd2fc93510c8e55efae809c5e2a9fe58141173b
Mathisnplus1 <mathis.achats74@gmail.com>
2024-08-15 17:02:50 +0200

    Update pMNIST et explications GroHess

--- Workspace/Core work/script_various_seeds.py
+++ Workspace/Core work/script_various_seeds.py
@@ -8,4 +8,4 @@ for global_seed in global_seeds :
     subprocess.run(["python", "script_greedy_HPO_old_thresholding.py"], 
                 input=json.dumps(global_seed).encode(),
                 capture_output=False,
-                check=True)
+                check=False)
